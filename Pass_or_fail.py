589737303c99800a5d5af8a6a1eb632c7160571d
shayan <shayan15052014@gmail.com>
2026-06-03 17:22:19 +0500

    p4

---
+++ Pass_or_fail.py
@@ -0,0 +1,6 @@
+marks = int(input("Enter your total marks in coding test: "))
+
+if marks > 40:
+    print("Congratulations, you are pass in coding test!")
+else:
+    print("sorry, you are failed in coding test")
