2b4dc86fd1aa5bd09c06859f0d0b7d15ed638f90
shayan <shayan15052014@gmail.com>
2026-06-05 15:16:47 +0500

    p5

--- Addition_from_1_till_entered_number.py
+++ Addition_from_1_till_entered_number.py
@@ -1,7 +1,10 @@
+number = int(input("Enter a number: "))
 
+for z in range(1,number + 1):
+    print(number,"+", z, "=",number + z)
 
 #=======================================================================================================
 
 num = int(input("Enter a number: "))
 for z in range(1,num,1):
-    print( )
+    print
